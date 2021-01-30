9fe33f1fba07b55e942945c2d7a141b7403a340d
Sharath221 <48938811+Sharath221@users.noreply.github.com>
2021-01-30 12:06:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
-variable "amis" {
-    description = "AMIs by region"
-    default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
-    }
-}
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "IGW_name" {}
@@ -26,7 +17,7 @@ variable Main_Routing_Table {}
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
   type = "list"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default = ["ap-south-1a", "ap-south-1b",]
 }
 variable "environment" { default = "dev" }
 variable "instance_type" {
